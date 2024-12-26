53ae1bfb2e711699bc5ec819ceb3d67b017ae989
Amit Sharma <amitsharma75700@gmail,com>
2024-12-26 17:49:04 +0530

    process.env.MONGO_URI is added

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://mrwhoisamit:vDkLlQEa6Gtft0ai@cluster0.jlrtl.mongodb.net/pastebin_db', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('MongoDB Connected'))
